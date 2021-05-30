e4c5783c9ac4b6ccafe762fbc84e7aefc6ed8944
camarvi <camarvi24@gmail.com>
2021-05-30 14:29:59 +0200

    Fin del proyecto

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -16,6 +16,13 @@ export const getUser = async (req : Request , res : Response): Promise<Response>
     return res.json(resultado);
 }
 
+export const deleteUser = async (req : Request , res : Response): Promise<Response> => {
+ 
+    //Esto equivale a SELECT * FROM USER
+    const resultado = await getRepository(User).delete(req.params.id);
+    return res.json(resultado);
+}
+
 export const createUser = async (req : Request , res : Response): Promise<Response> => {
 
     //Creo un nuevo usuario
@@ -23,4 +30,23 @@ export const createUser = async (req : Request , res : Response): Promise<Respon
     //Almaceno el nuevo usuario en la BD
     const result = await getRepository(User).save(newUser);
     return res.json(result);
+}
+
+export const updateUser = async (req : Request , res : Response): Promise<Response> => {
+
+    //Busco al Usuario en la base de datos
+    const user = await getRepository(User).findOne(req.params.id);
+    // Si existe el usuario
+    if (user) {
+        //Sustituyo los datos del usuario encontrado por los valores
+        //pasados por el req.body, esto lo hace en local
+        getRepository(User).merge(user, req.body);
+        // Guardo los datos en la base de datos
+        const results = await getRepository(User).save(user);
+        // Mando el resultado de la operacion
+        return res.json(results);
+    } else {
+        return res.status(404).json( { msg: 'Usuario no encontrado'});
+    }
+
 }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 const router = Router();
 
-import { getUsers, createUser, getUser } from '../controllers/user.controller';
+import { getUsers, createUser, getUser, updateUser, deleteUser } from '../controllers/user.controller';
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
 router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
     
 export default router;
